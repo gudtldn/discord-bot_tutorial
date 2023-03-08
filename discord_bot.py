91013955e4cabf5b876618e10da59f31b9bf9cc5
gudtldn <gudtldn1234@gmail.com>
2023-03-08 18:50:57 +0900

    init

---
+++ discord_bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=".",
+            intents=discord.Intents.all(),
+            application_id=...
+        )
+
+    async def on_ready(self):
+        print(f"{self.user.name}으로 로그인")
+        
+        await self.change_presence(
+            status=discord.Status.online,
+            activity=discord.Game("봇 테스트")
+        )
+    
+bot = Bot()
+
+with open("./token.txt", "r") as fr:
+    bot.run(fr.read())
